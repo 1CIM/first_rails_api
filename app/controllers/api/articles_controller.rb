c113749354fde07d85e03f84a789666d7052f78a
Christer <christerforsgren91@gmail.com>
2021-04-20 22:53:43 +0200

    add show article sad path

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -2,10 +2,17 @@ class Api::ArticlesController < ApplicationController
   def index
     articles = Article.all
     render json: { articles: articles }
+    binding.pry
   end
 
   def show
     article = Article.find(params['id'])
-    render json: { article: article }
+    render json: { article: article }, status: 200
+  end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    render json: { message: 'Unfortunately we can not find the article you are looking for.' }, status: 404
   end
 end
